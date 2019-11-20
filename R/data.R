284d1fc7e82f2c4ed9162443569de94626b09eb6
Andrew Lawrence <lawrenceajl@gmail.com>
2019-11-20 10:37:43 +0000

    DOC: clarification of bayes rate

--- R/data.R
+++ R/data.R
@@ -15,6 +15,10 @@
 #' \item{x.test}{20 Gaussian predictors. Test data, 1000 observations}
 #' \item{y.test}{Class membership for x. Test data, 1000 observations}
 #' \item{bayes.rate}{Classification accuracy in the complete sample given the
-#' rule used to generate the data (see: raw-data/blrsim.R)}
+#' rule used to generate the data (see: raw-data/blrsim.R).
+#'
+#' Note: calling this the bayes rate is a bit of a liberty
+#' as bayes rate is an error/loss while classification accuracy
+#' is the complement of such a error/loss.}
 #' }
 "blrsim"
