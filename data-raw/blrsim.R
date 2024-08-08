ff064ae3f80ade84ffa6c76918c74c38c793b268
Andrew Lawrence <lawrenceajl@gmail.com>
2024-08-08 16:07:59 +0100

    [BUG] input matrix should have colnames

--- data-raw/blrsim.R
+++ data-raw/blrsim.R
@@ -31,6 +31,7 @@ x <- MASS::mvrnorm(n = nobs_total,
                    mu = rep(0, nvar),
                    Sigma = xsig,
                    empirical = TRUE)
+colnames(x) <- paste0("V", 1:nvar)
 
 y <- ifelse(rowSums(x[, seq_len(ngood)]) > 0 +
               rnorm(n = nobs_total, mean = 0, sd = 2.5),
@@ -58,3 +59,4 @@ blrsim <- list(x = x,
                bayes.rate = bayes.rate)
 
 usethis::use_data(blrsim, overwrite = TRUE)
+
